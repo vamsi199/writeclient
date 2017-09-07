3a18c0db9b3c72213bdea8c268ce8278c8765d80
vamsi199 <vamsi.1991992@gmail.com>
2017-09-07 15:05:58 -0400

    first commit

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET", "http://localhost:8087/dog?color=blue&breed=no", nil)
+	fmt.Println(req)
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println("Errored when sending request to the server")
+		return
+	}
+
+	defer resp.Body.Close()
+	resp_body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.Status)
+	fmt.Println(string(resp_body))
+}
